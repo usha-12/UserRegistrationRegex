46903cff338afb5f2a46b29a6fcd6719fa87f933
usha-12 <usha12ahirwar@gmail.com>
2023-02-07 21:46:13 +0530

    Added: Chceck first name valide or not

--- src/com/brideglabz/userregistration/RegistrationProblem.java
+++ src/com/brideglabz/userregistration/RegistrationProblem.java
@@ -1,2 +1,10 @@
-package com.brideglabz.userregistration;public class RegistrationProblem {
+package com.brideglabz.userregistration;
+
+public class RegistrationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Regular Expressions program to validate PIN code!");
+        Validation validation=new Validation();
+        validation.checkFirstName();
+
+    }
 }
--- src/com/brideglabz/userregistration/Validation.java
+++ src/com/brideglabz/userregistration/Validation.java
@@ -1,2 +1,18 @@
-package com.brideglabz.userregistration;public class Validation {
+package com.brideglabz.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Validation {
+    Scanner sc=new Scanner(System.in);
+
+    public void checkFirstName(){
+        System.out.println("Enter First Name : ");
+        String firstName=sc.next();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{3,}",firstName)){
+            System.out.println("First Name is Valid...");
+        }else {
+            System.out.println("First Name is Invalid.....");
+        }
+    }
 }
